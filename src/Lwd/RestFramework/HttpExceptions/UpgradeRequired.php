0c5b670ad6a0fa0934b9277d0cc7c76ba7971667
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 23:25:11 -0700

    Update UpgradeRequired.php

--- src/Lwd/RestFramework/HttpExceptions/UpgradeRequired.php
+++ src/Lwd/RestFramework/HttpExceptions/UpgradeRequired.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server refuses to perform the request using the current protocol but might be willing to do so after the client upgrades to a different protocol. The server sends an Upgrade header in a 426 response to indicate the required protocol(s).
  */
-class UpgradeRequired extends BaseHttpException
+class UpgradeRequired extends BaseHttpClientException
 {
     const STATUS_CODE = 426;
+    const REASON_PHRASE = 'Upgrade Required';
 }
