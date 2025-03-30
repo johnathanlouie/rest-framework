e0cc71a1895107cd526ec08e01bfb33ddd4ec495
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 23:25:07 -0700

    Update UnsupportedMediaType.php

--- src/Lwd/RestFramework/HttpExceptions/UnsupportedMediaType.php
+++ src/Lwd/RestFramework/HttpExceptions/UnsupportedMediaType.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The media format of the requested data is not supported by the server, so the server is rejecting the request.
  */
-class UnsupportedMediaType extends BaseHttpException
+class UnsupportedMediaType extends BaseHttpClientException
 {
     const STATUS_CODE = 415;
+    const REASON_PHRASE = 'Unsupported Media Type';
 }
