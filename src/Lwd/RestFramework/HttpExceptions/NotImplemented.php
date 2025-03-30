42645e29a40f6b12827e270cd48e90bedda017e1
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:06:43 -0700

    Update NotImplemented.php

--- src/Lwd/RestFramework/HttpExceptions/NotImplemented.php
+++ src/Lwd/RestFramework/HttpExceptions/NotImplemented.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The request method is not supported by the server and cannot be handled. The only methods that servers are required to support (and therefore that must not return this code) are GET and HEAD.
  */
-class NotImplemented extends BaseHttpException
+class NotImplemented extends BaseHttpServerException
 {
     const STATUS_CODE = 501;
+    const REASON_PHRASE = 'Not Implemented';
 }
