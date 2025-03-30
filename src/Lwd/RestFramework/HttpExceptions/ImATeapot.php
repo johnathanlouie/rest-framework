1343d9fc33d56da700cf0fc9637dbd3a121db5fc
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:07:25 -0700

    Update ImATeapot.php

--- src/Lwd/RestFramework/HttpExceptions/ImATeapot.php
+++ src/Lwd/RestFramework/HttpExceptions/ImATeapot.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server refuses the attempt to brew coffee with a teapot.
  */
-class ImATeapot extends BaseHttpException
+class ImATeapot extends BaseHttpClientException
 {
     const STATUS_CODE = 418;
+    const REASON_PHRASE = "I'm a teapot";
 }
