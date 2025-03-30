5629721c425176fa239a12147fbbee2d491b3367
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:20:42 -0700

    Update HttpVersionNotSupported.php

--- src/Lwd/RestFramework/HttpExceptions/HttpVersionNotSupported.php
+++ src/Lwd/RestFramework/HttpExceptions/HttpVersionNotSupported.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The HTTP version used in the request is not supported by the server.
  */
-class HttpVersionNotSupported extends BaseHttpException
+class HttpVersionNotSupported extends BaseHttpServerException
 {
     const STATUS_CODE = 505;
+    const REASON_PHRASE = 'HTTP Version Not Supported';
 }
