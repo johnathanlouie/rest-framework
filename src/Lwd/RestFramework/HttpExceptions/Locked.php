c3808627ebcba807743bb9e70ed4e15404c66376
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:30:06 -0700

    Update Locked.php

--- src/Lwd/RestFramework/HttpExceptions/Locked.php
+++ src/Lwd/RestFramework/HttpExceptions/Locked.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The resource that is being accessed is locked.
  */
-class Locked extends BaseHttpException
+class Locked extends BaseHttpClientException
 {
     const STATUS_CODE = 423;
+    const REASON_PHRASE = 'Locked';
 }
