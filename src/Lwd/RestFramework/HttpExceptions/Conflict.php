0d606fd8ad0da4c3ecad9cc49bd9578995d3804c
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:07:00 -0700

    Update Conflict.php

--- src/Lwd/RestFramework/HttpExceptions/Conflict.php
+++ src/Lwd/RestFramework/HttpExceptions/Conflict.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This response is sent when a request conflicts with the current state of the server.
  */
-class Conflict extends BaseHttpException
+class Conflict extends BaseHttpClientException
 {
     const STATUS_CODE = 409;
+    const REASON_PHRASE = 'Conflict';
 }
