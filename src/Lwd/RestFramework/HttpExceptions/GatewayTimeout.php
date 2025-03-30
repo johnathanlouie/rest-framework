05247691b0cd6b718dbe3fcf6c4e2d8b3c9c7809
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:18:09 -0700

    Update GatewayTimeout.php

--- src/Lwd/RestFramework/HttpExceptions/GatewayTimeout.php
+++ src/Lwd/RestFramework/HttpExceptions/GatewayTimeout.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This error response is given when the server is acting as a gateway and cannot get a response in time.
  */
-class GatewayTimeout extends BaseHttpException
+class GatewayTimeout extends BaseHttpServerException
 {
     const STATUS_CODE = 504;
+    const REASON_PHRASE = 'Gateway Timeout';
 }
