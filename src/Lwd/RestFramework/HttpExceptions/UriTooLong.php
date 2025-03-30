65fb2451562be2636f2db04b0f16521a5357eae5
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 23:25:16 -0700

    Update UriTooLong.php

--- src/Lwd/RestFramework/HttpExceptions/UriTooLong.php
+++ src/Lwd/RestFramework/HttpExceptions/UriTooLong.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The URI requested by the client is longer than the server is willing to interpret.
  */
-class UriTooLong extends BaseHttpException
+class UriTooLong extends BaseHttpClientException
 {
     const STATUS_CODE = 414;
+    const REASON_PHRASE = 'URI Too Long';
 }
