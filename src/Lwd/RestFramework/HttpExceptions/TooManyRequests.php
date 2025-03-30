e1aff299712029c50b43cf13caf77311ea2337a2
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:55:51 -0700

    Update TooManyRequests.php

--- src/Lwd/RestFramework/HttpExceptions/TooManyRequests.php
+++ src/Lwd/RestFramework/HttpExceptions/TooManyRequests.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The user has sent too many requests in a given amount of time ("rate limiting").
  */
-class TooManyRequests extends BaseHttpException
+class TooManyRequests extends BaseHttpClientException
 {
     const STATUS_CODE = 429;
+    const REASON_PHRASE = 'Too Many Requests';
 }
