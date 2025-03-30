90237c4856e5c274046a86e39c1e1c551c203ece
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:29:58 -0700

    Update LengthRequired.php

--- src/Lwd/RestFramework/HttpExceptions/LengthRequired.php
+++ src/Lwd/RestFramework/HttpExceptions/LengthRequired.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * Server rejected the request because the Content-Length header field is not defined and the server requires it.
  */
-class LengthRequired extends BaseHttpException
+class LengthRequired extends BaseHttpClientException
 {
     const STATUS_CODE = 411;
+    const REASON_PHRASE = 'Length Required';
 }
