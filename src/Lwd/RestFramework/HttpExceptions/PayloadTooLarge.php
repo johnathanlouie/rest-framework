851544c0893b92c25643c39ad99840d545d1fb88
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:30:46 -0700

    Update PayloadTooLarge.php

--- src/Lwd/RestFramework/HttpExceptions/PayloadTooLarge.php
+++ src/Lwd/RestFramework/HttpExceptions/PayloadTooLarge.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * Request entity is larger than limits defined by server. The server might close the connection or return an Retry-After header field.
  */
-class PayloadTooLarge extends BaseHttpException
+class PayloadTooLarge extends BaseHttpClientException
 {
     const STATUS_CODE = 413;
+    const REASON_PHRASE = 'Payload Too Large';
 }
