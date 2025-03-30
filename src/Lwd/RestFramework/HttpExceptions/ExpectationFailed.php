acd35760e5c5105dc9f5f600b85c33ceefabd03a
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:07:10 -0700

    Update ExpectationFailed.php

--- src/Lwd/RestFramework/HttpExceptions/ExpectationFailed.php
+++ src/Lwd/RestFramework/HttpExceptions/ExpectationFailed.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This response code means the expectation indicated by the Expect request header field cannot be met by the server.
  */
-class ExpectationFailed extends BaseHttpException
+class ExpectationFailed extends BaseHttpClientException
 {
     const STATUS_CODE = 417;
+    const REASON_PHRASE = 'Expectation Failed';
 }
