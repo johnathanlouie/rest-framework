7a842ac996859381c3213d4e4a7a831d504998b9
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:30:38 -0700

    Update NetworkAuthenticationRequired.php

--- src/Lwd/RestFramework/HttpExceptions/NetworkAuthenticationRequired.php
+++ src/Lwd/RestFramework/HttpExceptions/NetworkAuthenticationRequired.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * Indicates that the client needs to authenticate to gain network access.
  */
-class NetworkAuthenticationRequired extends BaseHttpException
+class NetworkAuthenticationRequired extends BaseHttpServerException
 {
     const STATUS_CODE = 511;
+    const REASON_PHRASE = 'Network Authentication Required';
 }
