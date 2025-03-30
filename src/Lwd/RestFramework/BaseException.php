f6cc9f20a14d18434310bc0000da3d98bdf4760d
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 19:45:29 -0700

    Create BaseException.php

---
+++ src/Lwd/RestFramework/BaseException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Lwd\RestFramework;
+
+use Exception;
+
+/**
+ * Base exception class for the framework.
+ *
+ * This class serves as the base for all exceptions thrown within the framework.
+ * It extends the built-in Exception class to provide a custom exception handling
+ * mechanism.
+ */
+class BaseException extends Exception {}
