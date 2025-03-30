c11e60dc5176fe6aac11ef6c909decfe5cc6f239
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:52:48 -0700

    Update RequestHeaderFieldsTooLarge.php

--- src/Lwd/RestFramework/HttpExceptions/RequestHeaderFieldsTooLarge.php
+++ src/Lwd/RestFramework/HttpExceptions/RequestHeaderFieldsTooLarge.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server is unwilling to process the request because its header fields are too large. The request may be resubmitted after reducing the size of the request header fields.
  */
-class RequestHeaderFieldsTooLarge extends BaseHttpException
+class RequestHeaderFieldsTooLarge extends BaseHttpClientException
 {
     const STATUS_CODE = 431;
+    const REASON_PHRASE = 'Request Header Fields Too Large';
 }
