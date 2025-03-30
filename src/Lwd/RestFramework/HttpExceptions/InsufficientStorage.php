f3553d413db86b1b104afe2d77f9bdf689ad7f29
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:07:17 -0700

    Update InsufficientStorage.php

--- src/Lwd/RestFramework/HttpExceptions/InsufficientStorage.php
+++ src/Lwd/RestFramework/HttpExceptions/InsufficientStorage.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The method could not be performed on the resource because the server is unable to store the representation needed to successfully complete the request.
  */
-class InsufficientStorage extends BaseHttpException
+class InsufficientStorage extends BaseHttpServerException
 {
     const STATUS_CODE = 507;
+    const REASON_PHRASE = 'Insufficient Storage';
 }
