a3a9cbb01a6e09ccab25dd1b17a0e933f77a4a60
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:25:42 -0700

    Update ProxyAuthenticationRequired.php

--- src/Lwd/RestFramework/HttpExceptions/ProxyAuthenticationRequired.php
+++ src/Lwd/RestFramework/HttpExceptions/ProxyAuthenticationRequired.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This is similar to 401 Unauthorized but authentication is needed to be done by a proxy.
  */
-class ProxyAuthenticationRequired extends BaseHttpException
+class ProxyAuthenticationRequired extends BaseHttpClientException
 {
-    const STATUS_CODE = 401;
+    const STATUS_CODE = 407;
+    const REASON_PHRASE = 'Proxy Authentication Required';
 }
