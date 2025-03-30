5d997fefb65a14102661edf83d00d3bb6ea8be2c
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:06:25 -0700

    Update BadGateway.php

--- src/Lwd/RestFramework/HttpExceptions/BadGateway.php
+++ src/Lwd/RestFramework/HttpExceptions/BadGateway.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This error response means that the server, while working as a gateway to get a response needed to handle the request, got an invalid response.
  */
-class BadGateway extends BaseHttpException
+class BadGateway extends BaseHttpServerException
 {
     const STATUS_CODE = 502;
+    const REASON_PHRASE = 'Bad Gateway';
 }
