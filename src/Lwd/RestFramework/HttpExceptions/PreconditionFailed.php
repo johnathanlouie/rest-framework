c5988576b46a6a01a8bbad228d56ebd87b22df81
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:31:42 -0700

    Update PreconditionFailed.php

--- src/Lwd/RestFramework/HttpExceptions/PreconditionFailed.php
+++ src/Lwd/RestFramework/HttpExceptions/PreconditionFailed.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The client has indicated preconditions in its headers which the server does not meet.
  */
-class PreconditionFailed extends BaseHttpException
+class PreconditionFailed extends BaseHttpClientException
 {
     const STATUS_CODE = 412;
+    const REASON_PHRASE = 'Precondition Failed';
 }
