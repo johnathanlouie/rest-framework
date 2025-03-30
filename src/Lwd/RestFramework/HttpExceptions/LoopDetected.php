47b4511d32a2ab2360dc536e15cf63287b5e3721
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:21:49 -0700

    Update LoopDetected.php

--- src/Lwd/RestFramework/HttpExceptions/LoopDetected.php
+++ src/Lwd/RestFramework/HttpExceptions/LoopDetected.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server detected an infinite loop while processing the request.
  */
-class LoopDetected extends BaseHttpException
+class LoopDetected extends BaseHttpServerException
 {
     const STATUS_CODE = 508;
+    const REASON_PHRASE = 'Loop Detected';
 }
