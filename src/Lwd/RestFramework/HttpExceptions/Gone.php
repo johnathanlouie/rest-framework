12be4a3169f285039b4c175c7e6093035d9ebe8d
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:20:32 -0700

    Update Gone.php

--- src/Lwd/RestFramework/HttpExceptions/Gone.php
+++ src/Lwd/RestFramework/HttpExceptions/Gone.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This response is sent when the requested content has been permanently deleted from server, with no forwarding address. Clients are expected to remove their caches and links to the resource. The HTTP specification intends this status code to be used for "limited-time, promotional services". APIs should not feel compelled to indicate resources that have been deleted with this status code.
  */
-class Gone extends BaseHttpException
+class Gone extends BaseHttpClientException
 {
     const STATUS_CODE = 410;
+    const REASON_PHRASE = 'Gone';
 }
