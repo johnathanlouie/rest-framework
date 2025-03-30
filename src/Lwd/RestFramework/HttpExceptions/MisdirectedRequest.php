875c257a45bf91fa17210906f82b1bb5f418d89c
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:30:29 -0700

    Update MisdirectedRequest.php

--- src/Lwd/RestFramework/HttpExceptions/MisdirectedRequest.php
+++ src/Lwd/RestFramework/HttpExceptions/MisdirectedRequest.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The request was directed at a server that is not able to produce a response. This can be sent by a server that is not configured to produce responses for the combination of scheme and authority that are included in the request URI.
  */
-class MisdirectedRequest extends BaseHttpException
+class MisdirectedRequest extends BaseHttpClientException
 {
     const STATUS_CODE = 421;
+    const REASON_PHRASE = 'Misdirected Request';
 }
