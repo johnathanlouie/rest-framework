467653c75077aded97ab003e9f3d9b4e4443a5d5
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:07:52 -0700

    Update NotAcceptable.php

--- src/Lwd/RestFramework/HttpExceptions/NotAcceptable.php
+++ src/Lwd/RestFramework/HttpExceptions/NotAcceptable.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * This response is sent when the web server, after performing server-driven content negotiation, doesn't find any content that conforms to the criteria given by the user agent.
  */
-class NotAcceptable extends BaseHttpException
+class NotAcceptable extends BaseHttpClientException
 {
     const STATUS_CODE = 406;
+    const REASON_PHRASE = 'Not Acceptable';
 }
