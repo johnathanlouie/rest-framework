8668228d1e6d66ef4e79caee3dc01b26cc670ef1
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 23:25:03 -0700

    Update UnprocessableEntity.php

--- src/Lwd/RestFramework/HttpExceptions/UnprocessableEntity.php
+++ src/Lwd/RestFramework/HttpExceptions/UnprocessableEntity.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The request was well-formed but was unable to be followed due to semantic errors.
  */
-class UnprocessableEntity extends BaseHttpException
+class UnprocessableEntity extends BaseHttpClientException
 {
     const STATUS_CODE = 422;
+    const REASON_PHRASE = 'Unprocessable Entity';
 }
