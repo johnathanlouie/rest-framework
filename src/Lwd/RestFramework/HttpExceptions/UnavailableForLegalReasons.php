7b32a0676093f877fab2cb106a82d16e89077dca
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 23:24:59 -0700

    Update UnavailableForLegalReasons.php

--- src/Lwd/RestFramework/HttpExceptions/UnavailableForLegalReasons.php
+++ src/Lwd/RestFramework/HttpExceptions/UnavailableForLegalReasons.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The user agent requested a resource that cannot legally be provided, such as a web page censored by a government.
  */
-class UnavailableForLegalReasons extends BaseHttpException
+class UnavailableForLegalReasons extends BaseHttpClientException
 {
     const STATUS_CODE = 451;
+    const REASON_PHRASE = 'Unavailable For Legal Reasons';
 }
