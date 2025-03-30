2bfe1eac76c05bc730fc4a4bb8c7d015cf6f0b98
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:51:57 -0700

    Update RangeNotSatisfiable.php

--- src/Lwd/RestFramework/HttpExceptions/RangeNotSatisfiable.php
+++ src/Lwd/RestFramework/HttpExceptions/RangeNotSatisfiable.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The range specified by the Range header field in the request cannot be fulfilled. It's possible that the range is outside the size of the target URI's data.
  */
-class RangeNotSatisfiable extends BaseHttpException
+class RangeNotSatisfiable extends BaseHttpClientException
 {
     const STATUS_CODE = 416;
+    const REASON_PHRASE = 'Range Not Satisfiable';
 }
