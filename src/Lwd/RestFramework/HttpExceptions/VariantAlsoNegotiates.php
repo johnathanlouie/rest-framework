2ae6f3a7f7dacef434143f0243593c75ec96e534
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 23:25:22 -0700

    Update VariantAlsoNegotiates.php

--- src/Lwd/RestFramework/HttpExceptions/VariantAlsoNegotiates.php
+++ src/Lwd/RestFramework/HttpExceptions/VariantAlsoNegotiates.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server has an internal configuration error: the chosen variant resource is configured to engage in transparent content negotiation itself, and is therefore not a proper end point in the negotiation process.
  */
-class VariantAlsoNegotiates extends BaseHttpException
+class VariantAlsoNegotiates extends BaseHttpServerException
 {
     const STATUS_CODE = 506;
+    const REASON_PHRASE = 'Variant Also Negotiates';
 }
