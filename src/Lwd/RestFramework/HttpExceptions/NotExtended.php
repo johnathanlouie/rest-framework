948ea23fd8335f6e579d87a808e23a5bed6a6ca7
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:07:39 -0700

    Update NotExtended.php

--- src/Lwd/RestFramework/HttpExceptions/NotExtended.php
+++ src/Lwd/RestFramework/HttpExceptions/NotExtended.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * Further extensions to the request are required for the server to fulfill it.
  */
-class NotExtended extends BaseHttpException
+class NotExtended extends BaseHttpServerException
 {
     const STATUS_CODE = 510;
+    const REASON_PHRASE = 'Not Extended';
 }
