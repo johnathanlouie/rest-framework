c16b2115f59702ac209253085a36ec20bf68dee9
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:20:51 -0700

    Update InternalServerError.php

--- src/Lwd/RestFramework/HttpExceptions/InternalServerError.php
+++ src/Lwd/RestFramework/HttpExceptions/InternalServerError.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server has encountered a situation it does not know how to handle.
  */
-class InternalServerError extends BaseHttpException
+class InternalServerError extends BaseHttpServerException
 {
     const STATUS_CODE = 500;
+    const REASON_PHRASE = 'Internal Server Error';
 }
