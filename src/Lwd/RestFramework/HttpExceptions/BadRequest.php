f732dbc0d05031cec5590e5586aca87164f199db
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:06:54 -0700

    Update BadRequest.php

--- src/Lwd/RestFramework/HttpExceptions/BadRequest.php
+++ src/Lwd/RestFramework/HttpExceptions/BadRequest.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The server cannot or will not process the request due to something that is perceived to be a client error (e.g., malformed request syntax, invalid request message framing, or deceptive request routing).
  */
-class BadRequest extends BaseHttpException
+class BadRequest extends BaseHttpClientException
 {
     const STATUS_CODE = 400;
+    const REASON_PHRASE = 'Bad Request';
 }
