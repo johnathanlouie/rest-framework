08366c2ac54e5a685a1be999284970bd7719e4c6
Johnathan Louie <johnathanlouie88@gmail.com>
2025-03-29 22:30:22 -0700

    Update MethodNotAllowed.php

--- src/Lwd/RestFramework/HttpExceptions/MethodNotAllowed.php
+++ src/Lwd/RestFramework/HttpExceptions/MethodNotAllowed.php
@@ -5,7 +5,8 @@ namespace Lwd\RestFramework\HttpExceptions;
 /**
  * The request method is known by the server but is not supported by the target resource. For example, an API may not allow calling DELETE to remove a resource.
  */
-class MethodNotAllowed extends BaseHttpException
+class MethodNotAllowed extends BaseHttpClientException
 {
     const STATUS_CODE = 405;
+    const REASON_PHRASE = 'Method Not Allowed';
 }
