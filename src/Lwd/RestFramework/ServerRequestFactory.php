2ddd7bb7b2e4a0511abf1465d7d3c3e7a4cf50fc
Johnathan Louie <johnathanlouie88@gmail.com>
2025-04-01 20:54:39 -0700

    Update ServerRequestFactory.php

--- src/Lwd/RestFramework/ServerRequestFactory.php
+++ src/Lwd/RestFramework/ServerRequestFactory.php
@@ -15,17 +15,32 @@ class ServerRequestFactory implements ServerRequestFactoryInterface
     {
         // Use PHP superglobals as default values
         $serverParams = !empty($serverParams) ? $serverParams : $_SERVER;
-        
+
         // Get protocol version from server params or fall back to '1.1'
-        $protocolVersion = isset($serverParams['SERVER_PROTOCOL']) 
-            ? str_replace('HTTP/', '', $serverParams['SERVER_PROTOCOL']) 
+        $protocolVersion = isset($serverParams['SERVER_PROTOCOL'])
+            ? str_replace('HTTP/', '', $serverParams['SERVER_PROTOCOL'])
             : '1.1';
-        
+
+        // Extract headers from server params
+        $headers = [];
+        foreach ($serverParams as $key => $value) {
+            // Common non-HTTP_ headers - use case-insensitive comparison
+            if (strcasecmp($key, 'CONTENT_TYPE') === 0) {
+                $headers['Content-Type'] = $value;
+            } elseif (strcasecmp($key, 'CONTENT_LENGTH') === 0) {
+                $headers['Content-Length'] = $value;
+            } elseif (stripos($key, 'HTTP_') === 0) {
+                // Convert HTTP_HEADER_NAME to Header-Name
+                $name = str_replace('_', '-', substr($key, 5));
+                $headers[$name] = $value;
+            }
+        }
+
         return new ServerRequest(
             $method,
             $uri,
             $protocolVersion, // Use protocol version from server params
-            [], // Default headers
+            $headers, // Use headers extracted from server params
             null, // Default body 
             null, // Default request target
             $serverParams,
