3fe0d0e9ad3103cc763f32ca619f36cff92a26ac
Johnathan Louie <johnathanlouie88@gmail.com>
2025-04-02 11:53:21 -0700

    Create HelloWorldHandler.php

---
+++ src/Lwd/RestFramework/HelloWorldHandler.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Lwd\RestFramework;
+
+use Lwd\Http\Message\ResponseInterface;
+use Lwd\Http\Message\ResponseFactoryInterface;
+use Lwd\Http\Message\ServerRequestInterface;
+use Lwd\Http\Server\RequestHandlerInterface;
+
+/**
+ * A simple Hello World request handler.
+ * 
+ * This handler responds to all requests with a "Hello, World!" response.
+ * It implements the PSR-15 RequestHandlerInterface.
+ */
+class HelloWorldHandler implements RequestHandlerInterface
+{
+    /** @var ResponseFactoryInterface */
+    private $responseFactory;
+    
+    /**
+     * Constructs the handler with a response factory.
+     * 
+     * @param ResponseFactoryInterface $responseFactory Factory to create response objects
+     */
+    public function __construct($responseFactory)
+    {
+        $this->responseFactory = $responseFactory;
+    }
+    
+    /**
+     * Handles a server request and produces a response.
+     * 
+     * @param ServerRequestInterface $request The server request
+     * @return ResponseInterface The response
+     */
+    public function handle($request)
+    {
+        // Create a response
+        $response = $this->responseFactory->createResponse(200);
+        
+        // Get a writable body stream
+        $body = $response->getBody();
+        $body->write('Hello, World!');
+        
+        // Set content type header
+        $response = $response->withHeader('Content-Type', 'text/plain');
+        
+        return $response;
+    }
+}
